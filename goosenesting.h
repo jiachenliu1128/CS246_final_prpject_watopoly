6f7be5b764d6d6cab882d3d38624e191a7e70662
Andrew Liu <13122611073@163.com>
2023-06-17 21:18:43 +0800

    push

--- goosenesting.h
+++ goosenesting.h
@@ -5,10 +5,21 @@
 
 class GooseNesting: public NonProperty {
 public:
+
+    // ctor of GooseNesting
     GooseNesting(int position, std::string name): NonProperty(position, name) {}
+
+    // dtor of GooseNesting
     ~GooseNesting() {};
+
+    // int getNextMove() returns the steps of next move
+    // requires: None
+    // effects: None
     int getNextMove() override {return 0;}
 
+    // int action(Player& p, int cup) do the action of GooseNesting
+    // requires: None
+    // effects: None
     int action(Player& p, int cup) override {
         std::cout << std::endl << "==> You have arrived at Goose Nesting" << std::endl;
         std::cout << std::endl << "==> " << "Attacked by a flock of nesting geese!!! Run!!!" << std::endl;
